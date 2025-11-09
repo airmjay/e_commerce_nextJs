78837d986f699db861ddb2284a0e4cefb65a60ca
airmjay <abdulmojeedaliyu@gmail.com>
2025-11-10 00:05:54 +0100

    fix type for product api

--- app/api/product/route.tsx
+++ app/api/product/route.tsx
@@ -5,97 +5,110 @@ import { productSchema, z } from "../../zod/Validation";
 import { join } from "path";
 import { randomUUID } from "crypto";
 import { writeFile } from "fs/promises";
+import { ResultSetHeader } from "mysql2";
 
-const sanitizeInput = input => {
-    const sanitized = {};
-    for (const field in input) {
-        if (typeof input[field] === "string") {
-            sanitized[field] = sanitizeHtml(input[field], {
-                allowedTags: [],
-                allowedAttributes: {}
-            }).trim();
-        } else {
-            sanitized[field] = input[field];
-        }
+const sanitizeInput = (
+  input: Record<string, string | File | number | null>
+) => {
+  const sanitized: Record<string, string | File | number | null> = {};
+  for (const field in input) {
+    if (typeof input[field] === "string") {
+      sanitized[field] = sanitizeHtml(input[field], {
+        allowedTags: [],
+        allowedAttributes: {},
+      }).trim();
+    } else {
+      sanitized[field] = input[field];
     }
-    return sanitized;
+  }
+  return sanitized;
 };
 // CREATE: Insert a new item
-export async function POST(request) {
-    const formData = await request.formData();
-    console.log(formData, { error: "Only data is empty look 😂" });
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const get = (key: string): string | File | null => formData.get(key);
+  let imageFile: File | null | string | undefined = get("image");
+  if (imageFile && typeof imageFile === "object" && "name" in imageFile) {
+    imageFile = imageFile?.name;
+  }
+  const input = {
+    name: get("name"),
+    description: get("description"),
+    filename: formData.get("filename"),
+    price: Number(get("price")),
+    category: Number(get("category")),
+    unit: Number(get("unit")),
+    image: imageFile,
+    specification: formData.get("specification"),
+  };
 
-    const input = {
-        name: formData.get("name"),
-        description: formData.get("description"),
-        image: formData.get("image").name,
-        price: Number(formData.get("price")),
-        category: Number(formData.get("category")),
-        unit: Number(formData.get("unit")),
-        specification: formData.get("specification")
-    };
-    const file = formData.get("image");
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const fileuploadName =
-        randomUUID().slice(0, 8) +
-        Date.now() +
-        "." +
-        getFileExtension(input.image);
-    const filepath = join(process.cwd(), "public/uploads/", fileuploadName);
+  let fileUpload: null | undefined | string | File | Promise<ArrayBuffer> =
+    get("image");
+  if (fileUpload instanceof File) {
+    fileUpload = fileUpload.arrayBuffer();
+  }
 
-    await writeFile(filepath, buffer);
-    const sanitized = sanitizeInput(input);
-    try {
-        productSchema.parse(sanitized);
-        const [result] = await pool.query(
-            "INSERT INTO product (name, description, specification, unit, category_id, price, image ) VALUES (?, ?, ? , ? , ? , ? , ?)",
-            [
-                input.name,
-                input.description,
-                input.specification,
-                input.unit,
-                input.category,
-                input.price,
-                fileuploadName
-            ]
-        );
+  const bytes: string | ArrayBuffer | null = await fileUpload;
+  const buffer = Buffer.from(bytes as ArrayBuffer);
+  const fileuploadName =
+    randomUUID().slice(0, 8) +
+    Date.now() +
+    "." +
+    getFileExtension(input.image as string);
+  const filepath = join(process.cwd(), "public/uploads/", fileuploadName);
 
-        return NextResponse.json(
-            {
-                id: result.insertId,
-                name: input.name,
-                description: input.description
-            },
-            { status: 201 }
-        );
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return NextResponse.json({ errors: error.issues }, { status: 500 });
-        }
-        return NextResponse.json(
-            { error: "Internal server error" },
-            { status: 501 }
-        );
+  await writeFile(filepath, buffer);
+  const sanitized = sanitizeInput(input);
+  try {
+    productSchema.parse(sanitized);
+    const [result] = await pool.query<ResultSetHeader>(
+      "INSERT INTO product (name, description, specification, unit, category_id, price, image ) VALUES (?, ?, ? , ? , ? , ? , ?)",
+      [
+        input.name,
+        input.description,
+        input.specification,
+        input.unit,
+        input.category,
+        input.price,
+        fileuploadName,
+      ]
+    );
+
+    return NextResponse.json(
+      {
+        id: result.insertId,
+        name: input.name,
+        description: input.description,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ errors: error.issues }, { status: 500 });
     }
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 501 }
+    );
+  }
 }
 
 export async function GET() {
-    try {
-        const [rows] = await pool.query("SELECT * FROM product");
-        return NextResponse.json(rows);
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Failed to fetch items" },
-            { status: 500 }
-        );
-    }
+  try {
+    const [rows] = await pool.query("SELECT * FROM product");
+    return NextResponse.json(rows);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch items" + error },
+      { status: 500 }
+    );
+  }
 }
 
-function getFileExtension(filename) {
-    const parts = filename.split(".");
-    if (parts.length > 1) {
-        return parts.pop(); // Returns the last element (extension)
-    }
-    return ""; // No extension found
+function getFileExtension(filename: string) {
+  const parts = filename.split(".");
+  if (parts.length > 1) {
+    return parts.pop(); // Returns the last element (extension)
+  }
+  return ""; // No extension found
 }
