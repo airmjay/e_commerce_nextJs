fe13284c79d3c053b5e3304ae8357791a10808c1
airmjay <abdulmojeedaliyu@gmail.com>
2025-11-09 13:23:47 +0100

    still on typescript problems

--- app/admin/components/Button.tsx
+++ app/admin/components/Button.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 interface Props {
-    style: string;
-    text: string;
+  style: string;
+  text: string;
+  disabled: boolean;
 }
-const Button = ({ style, text,...rest }: Props) => {
-    return (
-        <button
-            className={`w-full button-color cursor-pointer hover:bg-blue-500 hover:text-gray-200 p-2 text-white outline-0 border-0 ${style}`}
-            {...rest}
-        >
-            {text}
-        </button>
-    );
+const Button = ({ style, text, disabled, ...rest }: Props) => {
+  return (
+    <button
+      disabled={disabled}
+      className={`w-full button-color cursor-pointer hover:bg-blue-500 hover:text-gray-200 p-2 text-white outline-0 border-0 ${style}`}
+      {...rest}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- app/admin/components/SelectWithConsumeApiFetch.tsx
+++ app/admin/components/SelectWithConsumeApiFetch.tsx
@@ -1,80 +1,100 @@
-"use client"
+"use client";
 import { SelectInput } from "./Input";
 import React, { useEffect, useState } from "react";
-import Category from '../../customer/products/components/Category';
-interface Props{
-   id : number;
-   readonly? : boolean | undefined
+import Category from "../../customer/products/components/Category";
+interface Props {
+  id?: number;
+  readonly?: boolean | undefined;
 }
-interface Category { 
-    name?: string;
-    id?: number;
+interface Category {
+  name?: string;
+  id?: number;
 }
-const SelectWithConsumeApiFetch = ({readonly, id, ...rest} : Props ) => {
-    const [isLoading, setIsloading] = useState(true);
-    const [category, setCategory] = useState<Category[]>([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const res  = await fetch(`http://localhost:3000/api/category`);
-            const categories = await res.json();
-            if (categories) {
-                setCategory(categories);
-                setIsloading(false);
-            }
-        };
-        fetchData();
-    }, []);
-    if (isLoading) return "loading";
-    return (
-        <SelectInput
-            readOnly={readonly}
-            label="Product Category"
-            placeholder="Enter Your Product Category"
-            {...rest}
-        >
-            {category.map(item =>
-                item.id == Number(id) ? (
-                    <option key={item.id} value={item.id} defaultValue={item.id}>
-                        {item.name}
-                    </option>
-                ) : (
-                    <option key={item.id} value={item.id}>
-                        {item.name} 
-                    </option>
-                )
-            )}
-        </SelectInput>
-    );
+interface Props {
+  onChange: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => void;
+  name: string;
+  error: string;
+  value: string;
+  label: string;
+  placeholder: string;
+}
+const SelectWithConsumeApiFetch = ({
+  id,
+  placeholder,
+  label,
+  ...rest
+}: Props) => {
+  const [isLoading, setIsloading] = useState(true);
+  const [category, setCategory] = useState<Category[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:3000/api/category`);
+      const categories = await res.json();
+      if (categories) {
+        setCategory(categories);
+        setIsloading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (isLoading) return "loading";
+  return (
+    <SelectInput label={label} placeholder={placeholder} {...rest}>
+      {category.map((item) =>
+        item.id == Number(id) ? (
+          <option key={item.id} value={item.id} defaultValue={item.id}>
+            {item.name}
+          </option>
+        ) : (
+          <option key={item.id} value={item.id}>
+            {item.name}
+          </option>
+        )
+      )}
+    </SelectInput>
+  );
 };
-const SelectWithConsumeApiFetchAll = ({ readonly, ...rest }: Props) => {
-    const [isLoading, setIsloading] = useState(true);
-    const [category, setCategory] = useState<Category[]>([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`http://localhost:3000/api/category`);
-            const categories  = await res.json();
-            if (categories) {
-                setCategory(categories);
-                setIsloading(false);
-            }
-        };
-        fetchData();
-    }, []);
-    if (isLoading) return "loading";
-    return (
-        <SelectInput
-            readOnly={readonly}
-            name="category"
-            label="Product Category"
-            placeholder="Enter Your Product Category"
-            {...rest}
-        >
-            {category.map(item => (
-                <option key={item.id} value={item.id}>
-                    {item.name} 
-                </option>
-            ))}
-        </SelectInput>
-    );
+const SelectWithConsumeApiFetchAll = ({
+  readonly,
+  onChange,
+  name,
+  placeholder,
+  label,
+  ...rest
+}: Props) => {
+  const [isLoading, setIsloading] = useState(true);
+  const [category, setCategory] = useState<Category[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:3000/api/category`);
+      const categories = await res.json();
+      if (categories) {
+        setCategory(categories);
+        setIsloading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (isLoading) return "loading";
+  return (
+    <SelectInput
+      onChange={onChange}
+      readOnly={readonly}
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      {...rest}
+    >
+      {category.map((item) => (
+        <option key={item.id} value={item.id}>
+          {item.name}
+        </option>
+      ))}
+    </SelectInput>
+  );
 };
 export { SelectWithConsumeApiFetchAll, SelectWithConsumeApiFetch };
